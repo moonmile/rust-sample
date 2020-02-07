f9eae88dca1d90b159a2a9cffbdfea24b2b87f23
moonmile <masuda@moonmile.net>
2020-02-07 11:09:16 +0900

    ch16 完了

--- src/ch16/sample-16-03/src/main.rs
+++ src/ch16/sample-16-03/src/main.rs
@@ -1,3 +1,19 @@
+use std::os::raw::c_char; 
+extern { 
+    fn puts(s: *const c_char) ;
+    fn strlen(s: *const c_char) -> usize; 
+}
+
+use std::ffi::CString ;
 fn main() {
-    println!("Hello, world!");
+    let s = "hello rust world." ;
+    let s_null_terminated = CString::new(s).unwrap() ;
+    unsafe {
+        puts( s_null_terminated.as_ptr());
+    }
+
+    let n = unsafe {
+        strlen( s_null_terminated.as_ptr())
+    } ;
+    println!("s.len is {}", n );
 }
