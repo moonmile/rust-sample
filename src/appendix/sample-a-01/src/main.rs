bb78d17a6905f9a03f59e17a2ecff3e6258afdae
Tomoaki Masuda <masuda@moonmile.net>
2020-02-20 21:45:09 +0900

    付録を追加

---
+++ src/appendix/sample-a-01/src/main.rs
@@ -0,0 +1,83 @@
+#[derive(Debug)]
+struct Person {
+    name: &'static str,
+    age: i32,
+}
+
+fn main1() {
+    // 変数のポインタを取得する
+    let a: i32 = 10 ;
+    let a_ptr: *const i32 = &a ;
+    println!("a is {:?}", a );
+    println!("a_ptr is {:?}", a_ptr );
+}
+
+fn main2() {
+    // 文字列のポインタを取得する
+    let a = "rust" ;
+    println!("a is {:?}", a );
+    println!("a_ptr is {:?}", a.as_ptr() );
+}
+
+fn main() {
+    let a: i32 = 100;
+    let a_ptr: *const i32 = &a;
+    println!("a is {:?}", a );
+    println!("a_ptr is {:?}", a_ptr );
+    // 変数b に copy する
+    let b = a ;
+    let b_ptr: *const i32 = &b;
+    println!("b is {:?}", b );
+    println!("b_ptr is {:?}", b_ptr );
+    // copy なので変数aが利用できる
+    println!("a is {:?}", a );
+}
+
+
+fn _main() {
+    println!("a から b へ move する");
+    let a = Person{name: "masuda", age: 50 };
+    let a_ptr: *const Person = &a;
+    println!("a is {:?}", a );
+    println!("a_ptr is {:?}", a_ptr );
+    println!("a.name.ptr is {:?}", a.name.as_ptr());
+    // 変数b に move する
+    let b = a ;
+    let b_ptr: *const Person = &b;
+    println!("b is {:?}", b );
+    println!("b_ptr is {:?}", b_ptr );
+    println!("b.name.ptr is {:?}", b.name.as_ptr());
+    // 既に move しているので使えない
+    // println!("a is {:?}", a );
+
+    println!("");
+    println!("a から b へ borrow する");
+    let a = Person{name: "masuda", age: 50 };
+    let a_ptr: *const Person = &a;
+    println!("a is {:?}", a );
+    println!("a_ptr is {:?}", a_ptr );
+    println!("a.name.ptr is {:?}", a.name.as_ptr());
+    // 変数b に borrow する
+    let b = &a ;
+    let b_ptr: *const Person = b;
+    println!("b is {:?}", b );
+    println!("b_ptr is {:?}", b_ptr );
+    println!("b.name.ptr is {:?}", b.name.as_ptr());
+    // 借用なので変数aが利用できる
+    println!("a is {:?}", a );
+
+    // 数値の場合はコピーされる
+    println!("");
+    println!("数値の場合は a から b へ copy する");
+    let a: i32 = 100;
+    let a_ptr: *const i32 = &a;
+    println!("a is {:?}", a );
+    println!("a_ptr is {:?}", a_ptr );
+    // 変数b に copy する
+    let b = a ;
+    let b_ptr: *const i32 = &b;
+    println!("b is {:?}", b );
+    println!("b_ptr is {:?}", b_ptr );
+    // copy なので変数aが利用できる
+    println!("a is {:?}", a );
+}
